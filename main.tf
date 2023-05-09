3313ddf6c21a99a8cca34e2b3056f8b19ff517dd
Divya Mathew <divyamatmail@gmail.com>
2023-05-09 10:13:22 +0530

    E0-49502: removed unwanted code

--- main.tf
+++ main.tf
@@ -40,13 +40,13 @@ module "eks_blueprints" {
 
   # List of Additional roles admin in the cluster
   # Comment this section if you ARE NOT at an AWS Event, as the TeamRole won't exist on your site, or replace with any valid role you want
-  map_roles = [
-    {
-      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
-      username = "ops-role" # The user name within Kubernetes to map to the IAM role
-      groups   = ["system:masters"] # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
-    }
-  ]
+#  map_roles = [
+#    {
+#      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
+#     username = "ops-role" # The user name within Kubernetes to map to the IAM role
+#      groups   = ["system:masters"] # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
+#    }
+#  ]
 
   # EKS MANAGED NODE GROUPS
   managed_node_groups = {
